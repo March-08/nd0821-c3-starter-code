c8ddd375f14034592b94901e88316315dc6c4243
Marcello Politi <mpoliti08@gmail.com>
2024-03-30 12:21:12 +0100

    fix: add examples in test file and ne actions

--- main.py
+++ main.py
@@ -108,6 +108,6 @@ def predict(data: Data):
         )
     except Exception as e:
         return Response(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_200_OK,
             content=f"something didn't work: {str(e)} ",
         )
